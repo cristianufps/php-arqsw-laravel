38f601d5e2c5faba5c03ead200f2ff288c8f57fd
andersonNB <andersonaugustonb@ufps.edu.co>
2021-05-04 21:59:58 -0500

    Primera Ruta

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/evento', [App\Http\Controllers\EventoController::class, 'index']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
